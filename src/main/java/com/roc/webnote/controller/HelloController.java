25d364b61cd1c764a9e0bb1f7ca553ac64c6ea72
Roc <cshijiel@foxmail.com>
2015-09-13 22:15:57 +0800

    add some info

--- src/main/java/com/roc/webnote/controller/HelloController.java
+++ src/main/java/com/roc/webnote/controller/HelloController.java
@@ -1,23 +1,16 @@
 package com.roc.webnote.controller;
 
-import com.roc.webnote.entity.Article;
-import com.roc.webnote.lib.QiNiuTools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
-import sun.misc.BASE64Decoder;
-
-import java.io.IOException;
-import java.net.URLDecoder;
-import java.util.UUID;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
 
-    private static final String imgFolder = "images/";
 
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
@@ -29,23 +22,10 @@ public class HelloController {
     }
 
 
-
+    // TODO should be removed
     @RequestMapping(value = "editor", method = RequestMethod.GET)
     public String editorRouter() {
         return "editor";
     }
-
-    @RequestMapping(value = "article")
-    @ResponseBody
-    public String addArticle(@ModelAttribute Article article) {
-        System.out.println(article.getTitle());
-        System.out.println(article.getCategory());
-        System.out.println(article.getContent());
-        System.out.println(article.getTagListStr());
-
-
-        return "OK";
-    }
-
 
 }
