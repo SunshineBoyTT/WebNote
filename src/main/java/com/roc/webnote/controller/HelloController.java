fe61efff54709cb09451dde6d3f846e1d1fdc62a
Roc <cshijiel@foxmail.com>
2015-09-14 13:13:04 +0800

    cookie

--- src/main/java/com/roc/webnote/controller/HelloController.java
+++ src/main/java/com/roc/webnote/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,12 +17,13 @@ public class HelloController {
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String printWelcome(@CookieValue("userCode") String userCode) {
+    public String printWelcome(@CookieValue(value = "userCode", required = false) String userCode) {
         logger.info("index");
-        if (null != userCode) {
+        if (StringUtils.isEmpty(userCode)) {
+            return "index";
+        } else {
             return "redirect:/article/list";
         }
-        return "index";
     }
 
 
